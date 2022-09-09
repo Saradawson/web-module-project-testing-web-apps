ea9cedbc969290dc9f688386176f2e9ad8bb6463
unknown <saradawsone@gmail.com>
2022-09-09 17:15:17 -0600

    render without error and render header

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,15 +5,17 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-
+    render(<ContactForm />)
 });
 
 test('renders the contact form header', () => {
-
+    render(<ContactForm />);
+    const header = screen.getByText(/contact form/i);
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-
+    
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
